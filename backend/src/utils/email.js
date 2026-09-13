12505d1128579d0f7944dfcffe3b707c9b2713c3
bipesh369 <juniorbipesh@gmail.com>
2026-09-13 11:43:12 +0000

    fix: set family option for nodemailer transport

--- backend/src/utils/email.js
+++ backend/src/utils/email.js
@@ -5,7 +5,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: Number(process.env.MAIL_PORT) || 587,
   secure: Number(process.env.MAIL_PORT) === 465,
-
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
